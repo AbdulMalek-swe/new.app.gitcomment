b22b9285c42b70bf2d79878088450de0a9d3ddbf
AbdulMalek-swe <abdulmalek.swe.585@gmail.com>
2023-05-04 15:25:12 +0600

    Your commit message here

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import About from './pages/About';
+import Footer from './pages/Footer';
 
 function App() {
   return (
     <div className="App">
        <About/>
+       <Footer/>
     </div>
   );
 }
